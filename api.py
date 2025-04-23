a7ce0f0a18e899fe5f2d58620d5de3a7130975b8
Tran Duong <anhduonghctdvp@gmail.com>
2025-04-23 16:55:07 +0700

    update: class Response

--- api.py
+++ api.py
@@ -78,8 +78,7 @@ class GameState(BaseModel):
     board: List[List[int]]
     current_player: int
     valid_moves: List[int]
-    game_over: bool
-    winner: int
+    is_new_game: bool
 
 class AIResponse(BaseModel):
     move: int
@@ -95,7 +94,7 @@ async def health_check():
 async def make_move(game_state: GameState) -> AIResponse:
     try:
         global old_board, str_state
-        if game_state.game_over:
+        if game_state.is_new_game:
             old_board = create_board()
             str_state = ""
         new_board = deepcopy(game_state.board)
